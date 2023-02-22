b65eb697bcdc62bfbc0eb292c2254ce71ae54cc2
Le Xuan Hung <lexuanhung1234576@gmail.com>
2023-02-22 14:03:34 +0700

    first commit

---
+++ Bangxoanoc.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F(i, a, b) for (int i = (a); i < (b); i++)
+
+int main()
+{
+    int x, y, x_, y_;
+    cin >> x >> y;
+    x_ = x;
+    y_ = y;
+    int a[x][y];
+    F(i, 0, x) F(j, 0, y) a[i][j] = 0;
+    int dx = 0, dy = 0, s = 0, direct = H;
+    while (x_ || y_)
+    {
+
+    }
+}
---
+++ LichKhamBenh.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int n; cin >> n;
+    int time[n], cho[n] = {0}, sum = 0;
+    for (int i = 0; i < n; i++) cin >> time[i];
+    sort(time, time + n);
+    for (int i = 0; i < n; i++)
+    {
+        cho[i + 1] = cho[i] + time[i];
+        sum += cho[i];
+    }
+    cout << sum << endl;
+    return 0;
+}
